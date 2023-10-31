88091ef304c6ea5d347cb6ce006cb1bda6645c69
kobinarth22@gmail.com <kobinarth22@gmail.com>
2023-10-31 09:57:34 +0530

    error in createItem.jsx

--- src/components/admin/createItem.jsx
+++ src/components/admin/createItem.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import "./CreateItem.css"; // Import the CSS file
+import "./createItem.css";
 
 function CreateItem() {
   const [item, setItem] = useState({
@@ -41,80 +41,31 @@ function CreateItem() {
           <div className="text-input">
             <div className="form-field">
               <label htmlFor="name">Name:</label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                placeholder="Name"
-                value={item.name}
-                onChange={handleInputChange}
-              />
+              <input type="text" id="name" name="name" placeholder="Name" value={item.name} onChange={handleInputChange} />
             </div>
             <div className="form-field">
               <label htmlFor="brand">Brand:</label>
-              <input
-                type="text"
-                id="brand"
-                name="brand"
-                placeholder="Brand"
-                value={item.brand}
-                onChange={handleInputChange}
-              />
+              <input type="text" id="brand" name="brand" placeholder="Brand" value={item.brand} onChange={handleInputChange} />
             </div>
             <div className="form-field">
               <label htmlFor="price">Price:</label>
-              <input
-                type="number"
-                id="price"
-                name="price"
-                placeholder="Price"
-                value={item.price}
-                onChange={handleInputChange}
-              />
+              <input type="number" id="price" name="price" placeholder="Price" value={item.price} onChange={handleInputChange} />
             </div>
             <div className="form-field">
               <label htmlFor="discount">Discount:</label>
-              <input
-                type="number"
-                id="discount"
-                name="discount"
-                placeholder="Discount"
-                value={item.discount}
-                onChange={handleInputChange}
-              />
+              <input type="number" id="discount" name="discount" placeholder="Discount" value={item.discount} onChange={handleInputChange} />
             </div>
             <div className="form-field">
               <label htmlFor="category">Category:</label>
-              <input
-                type="text"
-                id="category"
-                name="category"
-                placeholder="Category"
-                value={item.category}
-                onChange={handleInputChange}
-              />
+              <input type="text" id="category" name="category" placeholder="Category" value={item.category} onChange={handleInputChange} />
             </div>
           </div>
           <div className="image-input">
             <div className="form-field">
               <label htmlFor="image">Image:</label>
-              <input
-                type="file"
-                id="image"
-                name="image"
-                accept="image/*"
-                onChange={handleImageChange}
-              />
+              <input type="file" id="image" name="image" accept="image/*" onChange={handleImageChange} />
               <div className="create-item-image">
-                {item.image && (
-                  <img
-                    src={URL.createObjectURL(item.image)}
-                    alt="Image Preview"
-                    className="image-preview"
-                    width="200"
-                    height="150"
-                  />
-                )}
+                {item.image && <img src={URL.createObjectURL(item.image)} alt="Image Preview" className="image-preview" width="200" height="150" />}
               </div>
             </div>
           </div>
